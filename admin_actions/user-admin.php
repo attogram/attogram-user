476e18312848a92bc9c1ef39d149337005043cd9
Attogram <barberi.office@gmail.com>
2016-07-16 12:43:42 +0200

    spacing

--- admin_actions/user-admin.php
+++ admin_actions/user-admin.php
@@ -1,4 +1,4 @@
-<?php // Attogram Framework - User Module - User Admin v0.2.6
+<?php // Attogram Framework - User Module - User Admin v0.2.7
 
 namespace Attogram;
 
@@ -7,22 +7,22 @@ $this->pageHeader('👥 User Admin');
 print '<div class="container"><h1 class="squished">👥 User Admin</h1>';
 
 $this->database->tabler(
-  $table = 'user',
-  $tableId = 'id',
-  $nameSingular = 'user',
-  $namePlural = 'users',
-  $publicLink = false,
-  $col = array(
-    array('class'=>'col-md-1', 'title'=>'<code>ID</code>', 'key'=>'id'),
-    array('class'=>'col-md-5', 'title'=>'username', 'key'=>'username'),
-    array('class'=>'col-md-2', 'title'=>'password', 'key'=>'password'),
-    array('class'=>'col-md-2', 'title'=>'email', 'key'=>'email'),
-    array('class'=>'col-md-1', 'title'=>'level', 'key'=>'level'),
-  ),
-  $sql = 'SELECT * FROM user ORDER BY id',
-  $adminLink = '../users/',
-  $showEdit = true,
-  $perPage = 20
+    $table = 'user',
+    $tableId = 'id',
+    $nameSingular = 'user',
+    $namePlural = 'users',
+    $publicLink = false,
+    $col = array(
+        array('class'=>'col-md-1', 'title'=>'<code>ID</code>', 'key'=>'id'),
+        array('class'=>'col-md-5', 'title'=>'username', 'key'=>'username'),
+        array('class'=>'col-md-2', 'title'=>'password', 'key'=>'password'),
+        array('class'=>'col-md-2', 'title'=>'email', 'key'=>'email'),
+        array('class'=>'col-md-1', 'title'=>'level', 'key'=>'level'),
+    ),
+    $sql = 'SELECT * FROM user ORDER BY id',
+    $adminLink = '../users/',
+    $showEdit = true,
+    $perPage = 20
 );
 
 print '</div>';
