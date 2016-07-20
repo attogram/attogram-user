297f1244125f55639244de72353430e0bdc0cf9d
Attogram <barberi.office@gmail.com>
2016-07-20 23:08:07 +0200

    fix for null db, psr1/2

--- includes/AttogramUser.php
+++ includes/AttogramUser.php
@@ -1,4 +1,5 @@
-<?php // Attogram Framework - User Module - AttogramUser class v0.1.6
+<?php
+// Attogram Framework - User Module - AttogramUser class v0.1.7
 
 namespace Attogram;
 
@@ -7,12 +8,11 @@ namespace Attogram;
  */
 interface AttogramUserInterface {
 
-  public static function login( $psr3LoggerObject, $databaseObject );
+    public static function login($psr3LoggerObject, $databaseObject);
 
-  public static function logout();
+    public static function logout();
 
-  public static function isLoggedIn();
-
+    public static function isLoggedIn();
 }
 
 /**
@@ -22,63 +22,72 @@ interface AttogramUserInterface {
 class AttogramUser implements AttogramUserInterface
 {
 
-  /**
-   * login a user into the system
-   * @param  obj   $log       PSR-3 compliant logger object
-   * @param  obj   $database  The attogram database object
-   * @return bool
-   */
-  public static function login( $log, $database )
-  {
-    if( !isset($_POST['u']) || !isset($_POST['p']) || !$_POST['u'] || !$_POST['p'] ) {
-      $log->error('LOGIN: missing username or password');
-      return false;
+    /**
+     * login a user into the system
+     * @param  obj   $log       PSR-3 compliant logger object
+     * @param  obj   $database  The attogram database object
+     * @return bool
+     */
+    public static function login($log, $database)
+    {
+        if (!isset($_POST['u']) || !isset($_POST['p']) || !$_POST['u'] || !$_POST['p']) {
+            $log->error('LOGIN: missing username or password');
+            return false;
+        }
+        $bind[':u'] = $_POST['u'];
+        $bind[':p'] = $_POST['p'];
+        $user = $database->query(
+            'SELECT id, username, level, email FROM user WHERE username = :u AND password = :p',
+            $bind
+        );
+        // query failed
+        if (!$database->database || $database->database->errorCode() != '00000') {
+            $log->error('LOGIN: Login system offline');
+            return false;
+        }
+        // no user, or wrong password
+        if (!$user) {
+            $log->error('LOGIN: Invalid login');
+            return false;
+        }
+        // corrupt data
+        if (!sizeof($user) == 1) {
+            $log->error('LOGIN: Login system error');
+            return false;
+        }
+        $user = $user[0];
+        $_SESSION['attogram_id'] = $user['id'];
+        $_SESSION['AttogramUsername'] = $user['username'];
+        $_SESSION['attogram_level'] = $user['level'];
+        $_SESSION['attogram_email'] = $user['email'];
+        $log->debug('LOGIN: User Logged in');
+        return true;
     }
-    $bind[':u'] = $_POST['u'];
-    $bind[':p'] = $_POST['p'];
-    $user = $database->query('SELECT id, username, level, email FROM user WHERE username = :u AND password = :p', $bind );
 
-    if( $database->database->errorCode() != '00000' ) { // query failed
-      $log->error('LOGIN: Login system offline');
-      return false;
+    /**
+     * logout a user
+     */
+    public static function logout()
+    {
+        session_unset();
+        session_destroy();
+        session_start();
     }
-    if( !$user ) { // no user, or wrong password
-      $log->error('LOGIN: Invalid login');
-      return false;
-    }
-    if( !sizeof($user) == 1 ) { // corrupt data
-      $log->error('LOGIN: Login system error');
-      return false;
-    }
-    $user = $user[0];
-    $_SESSION['attogram_id'] = $user['id'];
-    $_SESSION['AttogramUsername'] = $user['username'];
-    $_SESSION['attogram_level'] = $user['level'];
-    $_SESSION['attogram_email'] = $user['email'];
-    $log->debug('LOGIN: User Logged in');
-    return true;
-  }
 
-  /**
-   * logout a user
-   */
-  public static function logout()
-  {
-    session_unset();
-    session_destroy();
-    session_start();
-  }
-
-  /**
-   * is a user logged into the system?
-   * @return bool
-   */
-  public static function isLoggedIn( )
-  {
-    if( isset($_SESSION['attogram_id']) && $_SESSION['attogram_id'] && isset($_SESSION['AttogramUsername']) && $_SESSION['AttogramUsername']) {
-      return true;
+    /**
+     * is a user logged into the system?
+     * @return bool
+     */
+    public static function isLoggedIn()
+    {
+        if (
+            isset($_SESSION['attogram_id'])
+            && $_SESSION['attogram_id']
+            && isset($_SESSION['AttogramUsername'])
+            && $_SESSION['AttogramUsername']
+        ) {
+            return true;
+        }
+        return false;
     }
-    return false;
-  }
-
 } // end class AttogramUser
